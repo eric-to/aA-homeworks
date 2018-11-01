678c19d3e432f88c874b762c906e21c41d97872e
Eric To <erto@berkeley.edu>
2018-11-01 00:46:34 -0700

    Finish W2D4 big_o homework

--- W2D4/big_o.rb
+++ W2D4/big_o.rb
@@ -50,7 +50,23 @@ def slow_dance(dir, arr)
   end
 end
 
+tiles_hash = {
+    "up" => 0,
+    "right-up" => 1,
+    "right"=> 2,
+    "right-down" => 3,
+    "down" => 4,
+    "left-down" => 5,
+    "left" => 6,
+    "left-up" => 7
+}
+
+def constant_dance(dir, arr)
+  arr[dir]
+end
+
 p sluggish_octo(fishies)
 p dominant_octo(fishies)
 p clever_octo(fishies)
 p slow_dance("right-down", tiles_array)
+p constant_dance("right-down", tiles_hash)
