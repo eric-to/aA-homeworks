bb004a14f6ab4ca8f9a8989db93150cc6a9179b6
Eric To <erto@berkeley.edu>
2018-11-01 00:44:15 -0700

    Write slow_dance method

--- W2D4/big_o.rb
+++ W2D4/big_o.rb
@@ -1,3 +1,6 @@
+fishies = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
+tiles_array = ["up", "right-up", "right", "right-down", "down", "left-down", "left",  "left-up" ]
+
 def sluggish_octo(arr)
   arr.each_with_index do |fish1, i|
     found_max = true
@@ -9,8 +12,45 @@ def sluggish_octo(arr)
   end
 end
 
-def dominant_octo(arr)
-  nil
+def merge_sort(arr)
+  return arr if arr.length <= 1
+  pivot = arr.length / 2
+  left = arr[0...pivot]
+  right = arr[pivot..-1]
+  merge_helper(merge_sort(left), merge_sort(right))
 end
 
-p dominant_octo(['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh'])
+def merge_helper(left, right)
+  combined_arr = []
+  until left.empty? || right.empty?
+    if left.first.length > right.first.length
+      combined_arr << right.shift
+    else
+      combined_arr << left.shift
+    end
+  end
+  combined_arr + left + right
+end
+
+def dominant_octo(arr)
+  merge_sort(arr)[-1]
+end
+
+def clever_octo(arr)
+  max_fish = ''
+  arr.each do |fish|
+    max_fish = fish if fish.length > max_fish.length
+  end
+  max_fish
+end
+
+def slow_dance(dir, arr)
+  arr.each_with_index do |move, idx|
+    return idx if move == dir
+  end
+end
+
+p sluggish_octo(fishies)
+p dominant_octo(fishies)
+p clever_octo(fishies)
+p slow_dance("right-down", tiles_array)
