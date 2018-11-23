a1f92d65a57a778eb43266dd88f82a3d6bbe530c
Eric To <erto@berkeley.edu>
2018-11-23 08:48:01 -0800

    Fix double input display for reader function

--- W5D5/callbacks.js
+++ W5D5/callbacks.js
@@ -14,7 +14,8 @@ const readline = require('readline');
 
 const reader = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
+  terminal: false
 });
 
 function teaAndBiscuits () {
