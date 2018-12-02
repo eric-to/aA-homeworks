f595db89cce7f8bf27afaf831de20c7bbd0afeb4
Eric To <erto@berkeley.edu>
2018-12-02 13:24:01 -0800

    Add starter files for redux exercise

--- W5D5/tea_and_biscuits.js
+++ W5D5/tea_and_biscuits.js
@@ -1,15 +1,3 @@
-// HAMMER TIME
-window.setTimeout(function () {
-  alert('HAMMERTIME');
-}, 5000);
-
-function hammerTime (time) {
-  window.setTimeout(function () {
-    alert(`${time} is HAMMER TIME!`);
-  });
-}
-
-// Teas and Biscuits
 const readline = require('readline');
 
 const reader = readline.createInterface({
@@ -35,20 +23,4 @@ function teaAndBiscuits () {
   });
 }
 
-// Dogs and Cats
-function Cat () {
-  this.name = 'Markov';
-  this.age = 3;
-}
-
-function Dog () {
-  this.name = 'Noodles';
-  this.age = 4;
-}
-
-Dog.prototype.chase = function (cat) {
-  console.log(`My name is ${this.name} and I'm chasing ${cat.name}! Woof!`)
-};
-
-const Markov = new Cat ();
-const Noodles = new Dog ();
+teaAndBiscuits();
---
+++ W6D5/skeleton/calculator.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  // your code here
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
