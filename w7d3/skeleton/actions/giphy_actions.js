7819c09c8a9b1de5bd0e2997007dc4ee0c537b2e
Eric To <erto@berkeley.edu>
2018-12-04 23:56:25 -0800

    Refactor for thunk action creator for fetching giphys

--- w7d3/skeleton/actions/giphy_actions.js
+++ w7d3/skeleton/actions/giphy_actions.js
@@ -1,10 +1,20 @@
 import * as APIUtil from '../util/api_util';
 
 export const RECEIVE_SEARCH_GIPHYS = 'RECEIVE_SEARCH_GIPHYS';
+export const REQUEST_SEARCH_GIPHYS = 'REQUEST_SEARCH_GIPHYS';
 
+// regular action creator, which returns a POJO
 export const receiveSearchGiphys = giphys => {
   return {
     type: RECEIVE_SEARCH_GIPHYS,
     giphys
   }
+};
+
+// thunk action creator, which will dispatch an action after successful api call
+// We want this action creator to dispatch receiveSearchGiphys after the Giphy API call is successful.
+export const fetchSearchGiphys = searchTerm => dispatch => {
+  return APIUtil.fetchSearchGiphys(searchTerm).then(giphys => {
+    receiveSearchGiphys(giphys.data);
+  }) 
 };
--- w7d3/skeleton/components/giphys_search.jsx
+++ w7d3/skeleton/components/giphys_search.jsx
@@ -2,4 +2,3 @@ import React from 'react';
 
 import { fetchSearchGiphys } from '../util/api_util';
 import GiphysIndex from './giphys_index';
-
